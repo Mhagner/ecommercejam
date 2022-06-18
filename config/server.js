f326a0d0b0a5e3d03094bc74c03f5fcf0334c085
Mhagner <m.hagner.sp@gmail.com>
2022-06-17 22:05:07 -0300

    initialize repo with mongodb

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '05c9b979c19006b6f924424218748f0d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ed32ad6c-1115-407a-98dd-b2d79d740635'
+};
